9419b5a9e48478aea7361a7c9cc9e8ef1b1d95b1
Bryan Chu <bryanchu10@gmail.com>
2022-10-14 11:00:07 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+        extensions: ['.js', '.vue'],
+      },
+    },
+    'import/core-modules': [
+      'vite',
+      '@vitejs/plugin-vue',
+      '@rushstack/eslint-patch/modern-module-resolution',
+    ],
+  },
+};
